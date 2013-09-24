ae6e2859e0045c691acc1c60680ed49d224f3907
Lakshman <acrlakshman@gmail.com>
2013-09-23 19:31:42 -0500

    Adding Eclipse project

---
+++ Shapes_Game/src/MyCircle.java
@@ -0,0 +1,26 @@
+import shapes.*;
+import java.awt.Color;
+
+public class MyCircle extends Circle {
+
+  // Put variables here!
+
+  @Override
+  public void setup() {
+    // Put code here!
+
+    // This code will be executed when your circle gets created.
+  }
+
+  @Override
+  public void update() {
+    // Put code here!
+
+    // This code will be executed once per frame.
+  }
+
+  public MyCircle() {
+    super();
+    setup();
+  }
+}
